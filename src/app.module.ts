7c88540a93f38ca0001e6ed2ed27b8404ca44c90
Fityandhiya Islam Nugroho <fityandhiya@gmail.com>
2022-02-20 11:58:12 +0700

    Init project configuration
    - Use fastify platform (@nestjs/platform-fastify) and remove express platform (@nestjs/platform-express).
    - Add @nestjs/config to using .env file.
    - Add .env.example file.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // This app use fastify.
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  // Configure app host and port.
+  const host = process.env.HOST;
+  const port = process.env.PORT;
+
+  // Start the app.
+  await app.listen(port, host);
+  console.log(`App run successfully on ${await app.getUrl()}.`);
 }
 bootstrap();
