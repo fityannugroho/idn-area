7a9aa08fe6fa53832da7e87b529b2f538e86ae84
Fityandhiya Islam Nugroho <fityandhiya@gmail.com>
2022-02-20 12:46:21 +0700

    Add API documentation with Swagger
    - Add @nestjs/swagger and fastify-swagger.
    - Setup API doc in main.ts.
    - Set endpoint for API doc in root path (docPath = '').
    - Update package info and version to v0.1.0.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +12,29 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
+
+  // Init API documentation with Swagger.
+  const docConfig = new DocumentBuilder()
+    .setTitle('Indonesia Area API')
+    .setVersion(process.env.npm_package_version)
+    .setDescription(
+      'API that provides information about Indonesia administrative area.',
+    )
+    .setLicense(
+      'MIT License',
+      'https://github.com/fityannugroho/idn-area/blob/master/LICENSE',
+    )
+    .setExternalDoc(
+      'See on GitHub',
+      'https://github.com/fityannugroho/idn-area',
+    )
+    .build();
+  // Create the API documentation.
+  const doc = SwaggerModule.createDocument(app, docConfig);
+  // Set the endpoint for API documentation.
+  const docPath = '';
+  // Setup the API documentation.
+  SwaggerModule.setup(docPath, app, doc);
 
   // Configure app host and port.
   const host = process.env.HOST;
