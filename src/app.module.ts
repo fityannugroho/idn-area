969e7d6df6b324692945513750dec14c4f36e380
Fityandhiya Islam Nugroho <fityandhiya@gmail.com>
2022-02-21 20:06:35 +0700

    Init mongodb config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [],
   providers: [],
 })
