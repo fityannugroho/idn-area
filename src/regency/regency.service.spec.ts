4c33d78ba71d99d730caad96158d203e95898c29
Fityan <fityannugroho@gmail.com>
2023-05-19 18:20:09 +0700

    test(regency): add missing module import

--- src/regency/regency.service.spec.ts
+++ src/regency/regency.service.spec.ts
@@ -2,13 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '~/src/common/services/prisma';
 import { RegencyController } from './regency.controller';
 import { RegencyService } from './regency.service';
+import { IslandModule } from '../island/island.module';
 
 describe('RegencyService', () => {
   let service: RegencyService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [],
+      imports: [IslandModule],
       controllers: [RegencyController],
       providers: [PrismaService, RegencyService],
     }).compile();
