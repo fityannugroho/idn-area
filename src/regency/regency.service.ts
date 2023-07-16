d4f90b2f4d35dea3b3677a1a3408a54599aeea46
Fityan <fityannugroho@gmail.com>
2023-07-17 06:45:17 +0700

    feat(regency): improve service & controller

--- src/regency/regency.service.ts
+++ src/regency/regency.service.ts
@@ -1,32 +1,28 @@
+import { CommonService, FindOptions } from '@/common/common.service';
+import { getDBProviderFeatures } from '@/common/utils/db';
+import { IslandService } from '@/island/island.service';
+import { PrismaService } from '@/prisma/prisma.service';
+import { SortOptions, SortService } from '@/sort/sort.service';
 import { Injectable } from '@nestjs/common';
 import { District, Island, Regency } from '@prisma/client';
-import { PrismaService } from '@/prisma/prisma.service';
-import { getDBProviderFeatures } from '@/common/utils/db';
-import { SortOptions, SortService } from '@/sort/sort.service';
-import { IslandService } from '../island/island.service';
 
 @Injectable()
-export class RegencyService {
-  private readonly sortService: SortService<Regency>;
+export class RegencyService implements CommonService<Regency> {
+  readonly sorter: SortService<Regency>;
 
   constructor(
     private readonly prisma: PrismaService,
     private readonly islandService: IslandService,
   ) {
-    this.sortService = new SortService<Regency>({
+    this.sorter = new SortService<Regency>({
       sortBy: 'code',
       sortOrder: 'asc',
     });
   }
 
-  /**
-   * If the name is empty, all regencies will be returned.
-   * Otherwise, it will only return the regencies with the matching name.
-   * @param name Filter by regency name (optional).
-   * @param sort The sort query (optional).
-   * @returns The array of regencies.
-   */
-  async find(name = '', sort?: SortOptions<Regency>): Promise<Regency[]> {
+  async find({ name, ...sortOptions }: FindOptions<Regency> = {}): Promise<
+    Regency[]
+  > {
     return this.prisma.regency.findMany({
       where: {
         name: {
@@ -36,16 +32,11 @@ export class RegencyService {
           }),
         },
       },
-      orderBy: this.sortService.object(sort),
+      orderBy: this.sorter.object(sortOptions),
     });
   }
 
-  /**
-   * Find a regency by its code.
-   * @param code The regency code.
-   * @returns An regency, or null if there are no match regency.
-   */
-  async findByCode(code: string): Promise<Regency> {
+  async findByCode(code: string): Promise<Regency | null> {
     return this.prisma.regency.findUnique({
       where: {
         code: code,
@@ -56,35 +47,34 @@ export class RegencyService {
   /**
    * Find all districts in a regency.
    * @param regencyCode The regency code.
-   * @param sort The sort query (optional).
-   * @returns Array of districts in the match regency, or `false` if there are no regency found.
+   * @param sortOptions The sort options.
+   * @returns An array of districts, or `null` if there are no match regency.
    */
   async findDistrics(
     regencyCode: string,
-    sort?: SortOptions<Regency>,
-  ): Promise<false | District[]> {
-    const districts = await this.prisma.regency
+    sortOptions?: SortOptions<Regency>,
+  ): Promise<District[] | null> {
+    return this.prisma.regency
       .findUnique({
         where: {
           code: regencyCode,
         },
       })
       .districts({
-        orderBy: this.sortService.object(sort),
+        orderBy: this.sorter.object(sortOptions),
       });
-
-    return districts ?? false;
   }
 
   /**
    * Find all islands in a regency.
    * @param regencyCode The regency code.
-   * @returns Array of islands in the match regency, or `false` if there are no regency found.
+   * @param sortOptions The sort options.
+   * @returns An array of islands, or `null` if there are no match regency.
    */
   async findIslands(
     regencyCode: string,
-    sort?: SortOptions<Island>,
-  ): Promise<false | Island[]> {
+    sortOptions?: SortOptions<Island>,
+  ): Promise<Island[] | null> {
     const islands = await this.prisma.regency
       .findUnique({
         where: {
@@ -92,11 +82,11 @@ export class RegencyService {
         },
       })
       .islands({
-        orderBy: this.islandService.sortService.object(sort),
+        orderBy: this.islandService.sortService.object(sortOptions),
       });
 
     if (!islands) {
-      return false;
+      return null;
     }
 
     return islands.map(this.islandService.addDecimalCoordinate);
