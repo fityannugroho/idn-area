9ea1fad72830ecdbfa4edcb5969afee4c64b1eb8
Fityan <fityannugroho@gmail.com>
2023-09-29 15:54:45 +0700

    feat: make CommonService.find support pagination

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -1,13 +1,16 @@
+import { PaginationQuery } from '@/common/dto/pagination.dto';
 import { SortOptions, SortService } from '@/sort/sort.service';
+import { PaginatedReturn } from './interceptor/paginate.interceptor';
 
 export type CommonData = Record<string, unknown> & {
   code: string;
   name: string;
 };
 
-export type FindOptions<T extends CommonData> = SortOptions<T> & {
-  name?: string;
-};
+export type FindOptions<T extends CommonData> = SortOptions<T> &
+  PaginationQuery & {
+    name?: string;
+  };
 
 export type CommonService<T extends CommonData> = {
   readonly sorter: SortService<T>;
@@ -15,8 +18,10 @@ export type CommonService<T extends CommonData> = {
   /**
    * If the name is empty, all data will be returned.
    * Otherwise, it will only return the data with the matching name.
+   *
+   * The result can also paginated by return an object with `data` and `meta` properties.
    */
-  find(options: FindOptions<T>): Promise<T[]>;
+  find(options: FindOptions<T>): Promise<T[] | PaginatedReturn<T>>;
 
   /**
    * Find a data by its code.
