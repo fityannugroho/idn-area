b961fc0fe2977ff11dfb280880d256e014c5d27a
Fityan <63894003+fityannugroho@users.noreply.github.com>
2024-11-20 19:53:51 +0700

    feat: move documentation from `/docs` into root `/` and remove `/docs`

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,31 +3,6 @@ import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
-  @Get()
-  @ApiOperation({ description: 'Base route of this API.' })
-  @ApiOkResponse({
-    schema: {
-      type: 'object',
-      properties: {
-        statusCode: { type: 'number', example: 200 },
-        message: { type: 'string' },
-        version: {
-          type: 'string',
-          description: 'The version of this API.',
-          example: '1.0.0',
-        },
-      },
-    },
-  })
-  async index() {
-    return {
-      statusCode: HttpStatus.OK,
-      message: 'Welcome to Indonesia Area API.',
-      version: process.env.npm_package_version,
-      docs: '/docs',
-    };
-  }
-
   @Get('health')
   @ApiOperation({ description: 'Check the health of this API.' })
   @ApiOkResponse({
